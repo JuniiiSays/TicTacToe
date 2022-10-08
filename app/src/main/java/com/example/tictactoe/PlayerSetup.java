af6793664b67a2d47334c3601dcfc895c3a986dd
Junaid Iqbal <Junaid.jigar761@gmail.com>
2022-10-08 08:21:31 +0500

    V2.01 : GameDisplay.java activity Added

--- app/src/main/java/com/example/tictactoe/PlayerSetup.java
+++ app/src/main/java/com/example/tictactoe/PlayerSetup.java
@@ -2,13 +2,32 @@ package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class PlayerSetup extends AppCompatActivity {
+
+    EditText player1, player2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_setup);
+
+        player1 = findViewById(R.id.editTextTextPersonName);
+        player2 = findViewById(R.id.editTextTextPersonName2);
+
     }
+
+    public void submitButtonClick(View view){
+        String player1Name = player1.getText().toString();
+        String player2Name = player2.getText().toString();
+
+        Intent intent = new Intent(this, GameDisplay.class);
+        intent.putExtra("PLAYERS_NAMES", new String[] {player1Name, player2Name});
+        startActivity(intent);
+    }
+
 }
